3602b95bbcd8901ee657ca0b8ab848d12685550b
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2022-08-09 23:26:34 -0400

    Create tip.py

---
+++ Problem Set 0/tip.py
@@ -0,0 +1,18 @@
+#tip calculator
+#https://cs50.harvard.edu/python/2022/psets/0/tip/
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+def dollars_to_float(d):
+  d = d.lstrip('$')
+  return(float(d))
+
+def percent_to_float(p):
+  p=p.rstrip('%')
+  p=float(p)
+  return(p/100)
+
+main()
