7a0656bb69fa93bc8a1fdad179aebeefcc433d45
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2022-08-09 23:23:08 -0400

    Update faces.py

--- Problem Set 0/faces.py
+++ Problem Set 0/faces.py
@@ -1,4 +1,5 @@
-#making faces
+#making faces 
+#https://cs50.harvard.edu/python/2022/psets/0/faces/
 face=input()
 if ':)' in face or ':(' in face:
   print(f'{face.replace(":)","🙂").replace(":(","🙁")}')
