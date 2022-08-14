31b16d215dca230d083cd3d30d9d1ea7af17ce52
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2022-08-14 18:03:21 -0400

    Update twttr.py

--- Problem_Set_2/twttr.py
+++ Problem_Set_2/twttr.py
@@ -2,23 +2,20 @@
 #https://cs50.harvard.edu/python/2022/psets/2/twttr/
 """
 I am going to do nested replace method
-First thing I did was take 'A','E','I','O','U' and make a list.  items=['A','E','I','O','U']
-Then i need to make them lower
+First thing I did was take 'A','E','I','O','U' and make a list
 
-items2=[x.lower() for x in items]
+items=['A','E','I','O','U','a','e','i','o','u']
 
-for i in items2:
-  print('.replace','(','"',i,'"',',' '"','"',')',sep='')
+for i in items:
+  print('.replace','(','"',i,'"',',' '"','"',')',sep='', end='')
 
-.replace("a","")
-.replace("e","")
-.replace("i","")
-.replace("o","")
-.replace("u","")
+Output looks like:
+
+.replace("A","").replace("E","").replace("I","").replace("O","").replace("U","").replace("a","").replace("e","").replace("i","").replace("o","").replace("u","")
 
 I then easily assembled my nested replcace methods.
 """
 #solution
 string = input('Input: ')
-rep = string.replace("a","").replace("e","").replace("i","").replace("o","").replace("u","")
+rep = string.replace("A","").replace("E","").replace("I","").replace("O","").replace("U","").replace("a","").replace("e","").replace("i","").replace("o","").replace("u","")
 print('Output: ',rep, sep='')
