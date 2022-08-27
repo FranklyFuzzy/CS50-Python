fcdeb979b891e10648513e591a2392e6490f0b6a
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2022-08-26 23:41:14 -0400

    Create taqueria.py

---
+++ Problem_Set_3/taqueria.py
@@ -0,0 +1,31 @@
+#taqueria.py
+#https://cs50.harvard.edu/python/2022/psets/3/taqueria/
+menu={
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+total=0
+
+while True:
+  try:
+    order=input('Item: ')
+    order=order.lower()
+    if order == "":
+      break
+    #total=0
+    for item in menu:
+      if order in item.lower():
+        food,price = item, menu[item]
+        if food.lower() == order:
+          total += price
+          print(f'${total:.2f}')
+  except EOFError:
+    break
