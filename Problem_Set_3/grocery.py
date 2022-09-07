ae35529f1452acc0edac3591ba296e604ae5b791
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2022-09-06 23:31:08 -0400

    Create grocery.py

---
+++ Problem_Set_3/grocery.py
@@ -0,0 +1,16 @@
+#grocery.py
+#https://cs50.harvard.edu/python/2022/psets/3/grocery/
+grocery = {}
+while True:
+    try:
+      item = input('').upper()
+      if item in grocery:
+        grocery[item] += 1
+      else:
+        grocery[item] = 1
+    except EOFError:
+        print()
+        break
+
+for i in sorted(grocery):
+	print(i, grocery[i])
